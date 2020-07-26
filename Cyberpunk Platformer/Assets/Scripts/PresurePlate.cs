1a328d5b3d6eced1f57b8e9563b0229e01a12ece
Antoni Garí <antonigarichia@gmail.com>
2020-07-26 12:43:52 +0100

    PresurePlateBasic

---
+++ Cyberpunk Platformer/Assets/Scripts/PresurePlate.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresurePlate : MonoBehaviour
+{
+    private GameObject topComponent;
+    [SerializeField] bool onPresurePlate;
+    [SerializeField] GameObject objectToToggle;
+
+    void Awake()
+    {
+        topComponent = GameObject.Find("TopSprite");
+        onPresurePlate = false;
+    }
+
+    private void Update()
+    {
+        if (onPresurePlate == true)
+        {
+            topComponent.SetActive(!onPresurePlate);
+            objectToToggle.SetActive(onPresurePlate);
+        }
+        else if(onPresurePlate == false)
+        {
+            topComponent.SetActive(!onPresurePlate);
+            objectToToggle.SetActive(onPresurePlate);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        onPresurePlate = true;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        onPresurePlate = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        onPresurePlate = false;
+    }
+}
